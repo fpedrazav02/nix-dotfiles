26285b26a2bfd971c3eb48715666f29f80c2fcce
Francisco Pedraza Vázquez <fpedrazav02@gmail.com>
2025-08-22 17:49:41 +0200

    Update home.nix

--- home.nix
+++ home.nix
@@ -1,119 +1,128 @@
 { config, pkgs, lib, ... }:
 
 {
-  # 🎯 Required by Home Manager
   home.stateVersion = "23.11";
 
-  # 📦 Global packages
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    initContent = ''
+      _noop() { :; }
+      zle -N insert-unambiguous-or-complete _noop
+      zle -N menu-search _noop
+      zle -N recent-paths _noop
+	  export TERM=xterm-256color
+    '';
+
+    loginExtra = ''
+      source ${config.home.profileDirectory}/etc/profile.d/hm-session-vars.sh
+    '';
+
+    plugins = [
+      {
+        name = "zsh-autocomplete";
+        src = pkgs.fetchFromGitHub {
+          owner  = "marlonrichert";
+          repo   = "zsh-autocomplete";
+          rev    = "762afacbf227ecd173e899d10a28a478b4c84a3f";
+          sha256 = "1357hygrjwj5vd4cjdvxzrx967f1d2dbqm2rskbz5z1q6jri1hm3";
+        };
+      }
+    ];
+
+    shellAliases = {
+      vim = "nvim";
+      f   = "fzf";
+      gs  = "git status";
+      ls  = "lsd";
+      ll  = "lsd -alh";
+    };
+
+    history.size = 10000;
+  };
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    bashrcExtra = ''
+      if command -v infocmp >/dev/null 2>&1; then
+        if ! infocmp "$TERM" >/dev/null 2>&1; then
+          export TERM=xterm-256color
+        fi
+      else
+        export TERM=xterm-256color
+      fi
+
+      if [[ $- == *i* ]]; then
+		source ${pkgs.blesh}/share/blesh/ble.sh
+      fi
+    '';
+  };
+
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+    enableBashIntegration = true;
+  };
+
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+    enableZshIntegration = true;
+    enableBashIntegration = true;
+  };
+
   home.packages = with pkgs; [
+    # TERM PKGS
     lsd
     fzf
     tmux
     ripgrep
+	blesh
     fd
     uv
     nerd-fonts.code-new-roman
-    zsh
     lazygit
-    direnv
 
-
-    # Languages
-    nodejs
+	# LANG
+    nodejs_20
     python3
 
-    # C/C++
+	# C / C++
     gcc
-    gnumake42
+    gnumake
     gdb
     clang-tools
-    
-    
-    # Formatters
+
+    # TOOLING
     black
     isort
     prettier
     stylua
 
-    # LSPs
+	# LSP
     basedpyright
     typescript-language-server
     lua-language-server
   ];
 
-  # 🐚 Zsh configuration
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-
-    # ✨ Built-in plugins
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-
-    # 🛠️ Manual extra initialization
-    initContent = ''
-      # Stub missing ZLE widgets
-      zle -N insert-unambiguous-or-complete true
-      zle -N menu-search              true
-      zle -N recent-paths             true
-
-      # Source syntax highlighter at the very end
-      source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-      source ${config.home.profileDirectory}/etc/profile.d/hm-session-vars.sh
-    '';
-
-    loginExtra = ''
-        source ${config.home.profileDirectory}/etc/profile.d/hm-session-vars.sh
-    '';
-
-
-    # 📦 External plugins loaded by Home Manager
-    plugins = [
-      {
-        name = "zsh-autocomplete";
-        src = pkgs.fetchFromGitHub {
-          owner = "marlonrichert";
-          repo  = "zsh-autocomplete";
-          rev   = "762afacbf227ecd173e899d10a28a478b4c84a3f";
-          sha256 = "1357hygrjwj5vd4cjdvxzrx967f1d2dbqm2rskbz5z1q6jri1hm3";
-        };
-      }
-      {
-        name = "powerlevel10k";
-        src  = pkgs.zsh-powerlevel10k;
-        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-      }
-      {
-        name = "powerlevel10k-config";
-        src  = ./p10k;
-        file = "p10k.zsh";
-      }
-    ];
-
-    # 📝 Aliases and history
-    shellAliases = {
-      vim = "nvim";
-      f   = "fzf";
-      gs  = "git status";
-      ll  = "ls -alh";
-      ls  = "lsd";
-    };
-    history.size = 10000;
-  };
-
-  # 🛠️ Environment variables
   home.sessionVariables = {
     EDITOR = "nvim";
+    SHELL  = "${pkgs.zsh}/bin/zsh";
   };
 
-   home.sessionPath = [
-    "/Users/frjpedra/.bin/"
+  home.sessionPath = [
+    "${config.home.homeDirectory}/.bin"
+    "${config.home.homeDirectory}/.local/bin"
   ];
+
   home.activation.fixZshrc = ''
-      sed -i 's/\r$//' "${config.home.homeDirectory}/.zshrc"
-    '';
+    sed -i 's/\r$//' "${config.home.homeDirectory}/.zshrc" || true
+  '';
 
-  # 📄 Dotfiles
   home.file.".tmux.conf".source = ./tmux.conf;
-
 }
