8c8bd010cb20be684710b957e2c3ab5f9d7e5a06
Francisco Pedraza Vázquez <fpedrazav02@gmail.com>
2025-10-28 16:10:53 +0100

    Update bash.nix

--- home/programs/bash.nix
+++ home/programs/bash.nix
@@ -7,5 +7,15 @@
     bashrcExtra = ''
       alias zsh=${pkgs.zsh}/bin/zsh
     '';
+
+    shellAliases = {
+      ll = "ls -l";
+      y = "yazi";
+      z = "zellij";
+      za = "zellij attach";
+      zs = "zellij -s";
+      cat = "bat";
+      ls = "lsd";
+    };
   };
 }
